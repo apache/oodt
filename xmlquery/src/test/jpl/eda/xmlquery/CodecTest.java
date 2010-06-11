21edd805aec3bb3ad567beb100cea1ec1134ee83
Sean William McCleese <smcclees@apache.org>
2010-06-11 20:38:55 +0000

    OODT-3 Progress: *.java files are finished
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oodt/trunk@953864 13f79535-47bb-0310-9956-ffa450edef68

--- xmlquery/src/test/jpl/eda/xmlquery/CodecTest.java
+++ xmlquery/src/test/jpl/eda/xmlquery/CodecTest.java
@@ -1,25 +1,20 @@
-// This software was developed by the the Jet Propulsion Laboratory, an operating division
-// of the California Institute of Technology, for the National Aeronautics and Space
-// Administration, an independent agency of the United States Government.
-// 
-// This software is copyrighted (c) 2000 by the California Institute of Technology.  All
-// rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without modification, is not
-// permitted under any circumstance without prior written permission from the California
-// Institute of Technology.
-//
-// THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR
-// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
-// THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// $Id: CodecTest.java,v 1.1.1.1 2004-03-02 19:37:17 kelly Exp $
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 
 package jpl.eda.xmlquery;
 
