18cfadbc9a7554f9862dfb867c24dbeaf9adde0f
Chris Mattmann <mattmann@apache.org>
2011-10-05 15:02:47 +0000

    - fix for OODT-34 Create an Action to Ingest an Ancillary File and for OODT-33 Refactor and Improve File Based Actions
    git-svn-id: https://svn.apache.org/repos/asf/oodt/trunk@1179252 13f79535-47bb-0310-9956-ffa450edef68

---
+++ crawler/src/main/java/org/apache/oodt/cas/crawl/action/FileSettings.java
@@ -0,0 +1,100 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.oodt.cas.crawl.action;
+
+//JDK imports
+import java.io.File;
+
+/**
+ * This class captures settings used in FileBased actions and the FileExists
+ * precondition. It specifies how to prepare a file string and provides
+ * consistent features across the aforementioned. This preparation supports such
+ * converting from something such as image.jpg to cool_image_ancillary.png. In
+ * this scenario the filePrefix="cool_", fileSuffix="_ancillary",
+ * fileExtension="png", and keepExistingExtension=true. This conversion could
+ * take place by passing calling the getPreparedFileString method.
+ * 
+ * @author pramirez
+ * @author mattmann
+ * 
+ */
+public class FileSettings {
+  private String filePrefix;
+  private String fileSuffix;
+  private String fileExtension;
+  private boolean keepExistingExtension;
+
+  public FileSettings() {
+    this.keepExistingExtension = true;
+  }
+
+  public void setFilePrefix(String filePrefix) {
+    this.filePrefix = filePrefix;
+  }
+
+  public void setFileSuffix(String fileSuffix) {
+    this.fileSuffix = fileSuffix;
+  }
+
+  public void setFileExtension(String fileExtension) {
+    this.fileExtension = fileExtension;
+  }
+
+  public void setKeepExistingExtension(boolean keepExistingExtension) {
+    this.keepExistingExtension = keepExistingExtension;
+  }
+
+  public String getPreparedFileString(File file) {
+    StringBuffer fileString = new StringBuffer();
+
+    if (file.getParent() != null) {
+      fileString.append(file.getParent());
+      fileString.append(System.getProperties().getProperty("file.separator",
+          "/"));
+    }
+
+    if (filePrefix != null) {
+      fileString.append(filePrefix);
+    }
+
+    String existingExtension = "";
+    String filename = file.getName();
+    int existingIndex = filename.lastIndexOf(".");
+    if (existingIndex != -1) {
+      existingExtension = filename.substring(existingIndex);
+      filename = filename.substring(0, existingIndex);
+    }
+
+    fileString.append(filename);
+
+    if (fileSuffix != null) {
+      fileString.append(fileSuffix);
+    }
+
+    if (keepExistingExtension) {
+      fileString.append(existingExtension);
+    }
+
+    if (fileExtension != null) {
+      fileString.append(".");
+      fileString.append(fileExtension);
+    }
+
+    return fileString.toString();
+  }
+}
---
+++ crawler/src/main/java/org/apache/oodt/cas/crawl/action/IngestAncillary.java
@@ -0,0 +1,209 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.oodt.cas.crawl.action;
+
+//JDK imports
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+//OODT imports
+import org.apache.oodt.cas.crawl.structs.exceptions.CrawlerActionException;
+import org.apache.oodt.cas.filemgr.ingest.Ingester;
+import org.apache.oodt.cas.filemgr.ingest.StdIngester;
+import org.apache.oodt.cas.filemgr.metadata.CoreMetKeys;
+import org.apache.oodt.cas.metadata.Metadata;
+import org.apache.oodt.cas.metadata.util.PathUtils;
+
+//Spring imports
+import org.springframework.beans.factory.annotation.Required;
+
+/**
+ * This action allows the crawler to ingest an ancillary file while crawling
+ * other files. For example, if the crawler is configured to pick up images then
+ * this action could be used to pick up the thumbnails. The ancillary file will
+ * be submitted to the file manager given the product type specified and an
+ * identifier placed in the metadata of the file being ingested. One will have
+ * to set up the product type in the file manager policy to support this action.
+ * This action does not perform metadata extraction and if this is required one
+ * should consider using a server side metadata extractor configured with the
+ * file manager.
+ * 
+ * @author pramirez
+ * @author mattmann
+ * 
+ */
+public class IngestAncillary extends FileBasedAction {
+  private String fileManagerUrl;
+  private String dataTransferService;
+  private String relatedKey;
+  private String productType;
+  private Map<String, List<String>> ancillaryMetadata;
+  private List<String> copyKeys;
+  private List<String> replaceDynamicKeys;
+  private List<String> writeBackKeys;
+  private String writeBackKeyPrefix;
+
+  public IngestAncillary() {
+    super();
+  }
+
+  @Override
+  public boolean performFileAction(File actionFile, Metadata metadata)
+      throws CrawlerActionException {
+    Metadata ingestMetadata = new Metadata();
+    if (ancillaryMetadata != null) {
+      for (String key : this.ancillaryMetadata.keySet()) {
+        for (String value : this.ancillaryMetadata.get(key)) {
+          ingestMetadata.addMetadata(key, PathUtils.replaceEnvVariables(value));
+        }
+      }
+    }
+
+    if (copyKeys != null) {
+      for (String copyKey : copyKeys) {
+        if (metadata.containsKey(copyKey)) {
+          ingestMetadata.addMetadata(copyKey, metadata.getAllMetadata(copyKey));
+        }
+      }
+    }
+
+    if (ingestMetadata.getMetadata(CoreMetKeys.FILE_LOCATION) == null) {
+      ingestMetadata.addMetadata(CoreMetKeys.FILE_LOCATION,
+          actionFile.getParent());
+    }
+    if (ingestMetadata.getMetadata(CoreMetKeys.FILENAME) == null) {
+      ingestMetadata.addMetadata(CoreMetKeys.FILENAME, actionFile.getName());
+    }
+    if (ingestMetadata.getMetadata(CoreMetKeys.PRODUCT_NAME) == null) {
+      ingestMetadata.addMetadata(CoreMetKeys.PRODUCT_NAME, actionFile.getName()
+          .substring(0, actionFile.getName().indexOf(".")));
+    }
+    if (ingestMetadata.getMetadata(CoreMetKeys.PRODUCT_TYPE) == null) {
+      ingestMetadata.addMetadata(CoreMetKeys.PRODUCT_TYPE, productType);
+    }
+
+    try {
+      Ingester ingester = new StdIngester(dataTransferService);
+      String identifier = ingester.ingest(new URL(this.fileManagerUrl),
+          actionFile, ingestMetadata);
+      if (identifier != null) {
+        LOG.info("Succesfully ingested ancillary file "
+            + actionFile.getAbsolutePath() + " with identifier " + identifier);
+      } else {
+        LOG.severe("Failed to ingest ancillary file "
+            + actionFile.getAbsolutePath()
+            + " identifer was not returned from file manager");
+      }
+
+      if (relatedKey != null) {
+        metadata.addMetadata(relatedKey, identifier);
+      }
+
+      if (writeBackKeys != null) {
+        if (writeBackKeyPrefix == null) {
+          writeBackKeyPrefix = "";
+        }
+        ingestMetadata.addMetadata(writeBackKeyPrefix + CoreMetKeys.PRODUCT_ID,
+            identifier);
+        for (String writeBackKey : writeBackKeys) {
+          metadata.addMetadata(writeBackKeyPrefix + writeBackKey,
+              ingestMetadata.getAllMetadata(writeBackKey));
+        }
+      }
+
+      if (replaceDynamicKeys != null) {
+        for (String replaceDynamicKey : replaceDynamicKeys) {
+          String value = PathUtils.replaceEnvVariables(
+              metadata.getMetadata(replaceDynamicKey), metadata);
+          metadata.removeMetadata(replaceDynamicKey);
+          metadata.addMetadata(replaceDynamicKey, value);
+        }
+      }
+
+    } catch (Exception ex) {
+      LOG.severe("Failed to ingest ancillary file "
+          + actionFile.getAbsolutePath());
+      LOG.severe(ex.getMessage());
+      return false;
+    }
+
+    return true;
+  }
+
+  public void setReplaceDynamicKeys(List<String> replaceDynamicKeys) {
+    this.replaceDynamicKeys = replaceDynamicKeys;
+  }
+
+  @Required
+  public void setFileManagerUrl(String fileManagerUrl) {
+    this.fileManagerUrl = fileManagerUrl;
+  }
+
+  @Required
+  public void setDataTransferService(String dataTransferService) {
+    this.dataTransferService = dataTransferService;
+  }
+
+  public void setRelatedKey(String relatedKey) {
+    this.relatedKey = relatedKey;
+  }
+
+  public void setAncillaryMetadata(Map<String, List<String>> ancillaryMetadata) {
+    this.ancillaryMetadata = ancillaryMetadata;
+  }
+
+  @Required
+  public void setProductType(String productType) {
+    this.productType = productType;
+  }
+
+  public void setCopyKeys(List<String> copyKeys) {
+    this.copyKeys = copyKeys;
+  }
+
+  public void setWriteBackKeys(List<String> writeBackKeys) {
+    this.writeBackKeys = writeBackKeys;
+  }
+
+  public void setWriteBackKeyPrefix(String writeBackKeyPrefix) {
+    this.writeBackKeyPrefix = writeBackKeyPrefix;
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 6) {
+      System.out
+          .println("Usage: java "
+              + IngestAncillary.class.getName()
+              + " <serviceUrl> <transferServiceClass> <productFile> <fileSuffix> <relatedKey> <productType");
+      System.exit(-1);
+    }
+    IngestAncillary ingest = new IngestAncillary();
+    ingest.setFileManagerUrl(args[0]);
+    ingest.setDataTransferService(args[1]);
+    File product = new File(args[2]);
+    ingest.setFileSuffix(args[3]);
+    ingest.setRelatedKey(args[4]);
+    ingest.setProductType(args[5]);
+    Metadata metadata = new Metadata();
+    ingest.performAction(product, metadata);
+    System.out.println("Ingested " + args[5] + ": "
+        + metadata.getMetadata(args[4]));
+  }
+}
