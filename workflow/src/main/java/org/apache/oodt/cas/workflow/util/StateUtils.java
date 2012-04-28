3c3d4292c97fae485ff3d0912b277f55bc2808ab
Chris Mattmann <mattmann@apache.org>
2012-04-28 03:52:01 +0000

    - progress towards OODT-215 and OODT-310: wengine integration. These classes now compile and don't have any errors: wow. Just focusing on integration right now.
    git-svn-id: https://svn.apache.org/repos/asf/oodt/trunk@1331672 13f79535-47bb-0310-9956-ffa450edef68

---
+++ workflow/src/main/java/org/apache/oodt/cas/workflow/util/StateUtils.java
@@ -0,0 +1,168 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.oodt.cas.workflow.util;
+
+//JDK imports
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+
+//OODT imports
+import org.apache.oodt.cas.workflow.engine.TaskProcessor;
+import org.apache.oodt.cas.workflow.engine.WorkflowProcessor;
+import org.apache.oodt.cas.workflow.lifecycle.WorkflowLifecycleStage;
+import org.apache.oodt.cas.workflow.lifecycle.WorkflowState;
+
+/**
+ * 
+ * Utilities for working with WorkflowState.
+ * 
+ * @author bfoster
+ * @author mattmann
+ * @version $Revision$
+ */
+public class StateUtils {
+
+  private StateUtils() {
+  }
+
+  public static boolean allOfGivenState(
+      List<WorkflowProcessor> workflowProcessors,
+      Class<? extends WorkflowState> stateClass) {
+    for (WorkflowProcessor workflowProcessor : workflowProcessors)
+      if (stateClass.isAssignableFrom(workflowProcessor.getState().getClass()))
+        return false;
+    return true;
+  }
+
+  public static boolean containsGivenState(
+      List<WorkflowProcessor> workflowProcessors,
+      Class<? extends WorkflowState> stateClass) {
+    for (WorkflowProcessor workflowProcessor : workflowProcessors)
+      if (stateClass.isAssignableFrom(workflowProcessor.getState().getClass()))
+        return true;
+    return false;
+  }
+
+  public static boolean allOfGivenCategory(
+      List<WorkflowProcessor> workflowProcessors,
+      WorkflowLifecycleStage category) {
+    for (WorkflowProcessor workflowProcessor : workflowProcessors)
+      if (!workflowProcessor.getState().getCategory().equals(category))
+        return false;
+    return true;
+  }
+
+  public static boolean containsTaskInGivenCategory(
+      WorkflowProcessor workflowProcessor, WorkflowLifecycleStage category) {
+    Stack<WorkflowProcessor> stack = new Stack<WorkflowProcessor>();
+    stack.add(workflowProcessor);
+    while (!stack.empty()) {
+      WorkflowProcessor wp = stack.pop();
+      if (wp instanceof TaskProcessor
+          && wp.getState().getCategory().equals(category))
+        return true;
+      stack.addAll(wp.getSubProcessors());
+    }
+    return false;
+  }
+
+  public static boolean constainsGivenCategory(
+      List<WorkflowProcessor> workflowProcessors,
+      WorkflowLifecycleStage category) {
+    for (WorkflowProcessor workflowProcessor : workflowProcessors)
+      if (workflowProcessor.getState().getCategory().equals(category))
+        return true;
+    return false;
+  }
+
+  public static List<WorkflowProcessor> getWorkflowProcessorsOfGivenState(
+      List<WorkflowProcessor> workflowProcessors,
+      Class<? extends WorkflowState> stateClass) {
+    List<WorkflowProcessor> returnProcessors = new Vector<WorkflowProcessor>();
+    for (WorkflowProcessor workflowProcessor : workflowProcessors)
+      if (stateClass.isAssignableFrom(workflowProcessor.getState().getClass()))
+        returnProcessors.add(workflowProcessor);
+    return returnProcessors;
+  }
+
+  public static List<WorkflowProcessor> getWorkflowProcessorsOfGivenCategory(
+      List<WorkflowProcessor> workflowProcessors,
+      WorkflowLifecycleStage category) {
+    List<WorkflowProcessor> returnProcessors = new Vector<WorkflowProcessor>();
+    for (WorkflowProcessor workflowProcessor : workflowProcessors)
+      if (workflowProcessor.getState().getCategory().equals(category))
+        returnProcessors.add(workflowProcessor);
+    return returnProcessors;
+  }
+
+  public static List<WorkflowProcessor> getWorkflowProcessorsNotOfGivenState(
+      List<WorkflowProcessor> workflowProcessors,
+      Class<? extends WorkflowState> stateClass) {
+    List<WorkflowProcessor> returnProcessors = new Vector<WorkflowProcessor>();
+    for (WorkflowProcessor workflowProcessor : workflowProcessors)
+      if (!stateClass.isAssignableFrom(workflowProcessor.getState().getClass()))
+        returnProcessors.add(workflowProcessor);
+    return returnProcessors;
+  }
+
+  public static List<WorkflowProcessor> getWorkflowProcessorsNotOfGivenCategory(
+      List<WorkflowProcessor> workflowProcessors,
+      WorkflowLifecycleStage category) {
+    List<WorkflowProcessor> returnProcessors = new Vector<WorkflowProcessor>();
+    for (WorkflowProcessor workflowProcessor : workflowProcessors)
+      if (!workflowProcessor.getState().getCategory().equals(category))
+        returnProcessors.add(workflowProcessor);
+    return returnProcessors;
+  }
+
+  public static WorkflowLifecycleStage getCategoryByName(
+      List<WorkflowState> states, String name) {
+    for (WorkflowState state : states) {
+      if (state.getCategory().toString().toLowerCase()
+          .equals(name.toLowerCase()))
+        return state.getCategory();
+    }
+    return null;
+  }
+
+  public static WorkflowState getStateByName(List<WorkflowState> states,
+      String name) {
+    String[] splitName = name.split("[\\(\\)]{1}");
+    String revertState = null;
+    if (splitName.length == 2) {
+      name = splitName[0];
+      revertState = splitName[1];
+    }
+    WorkflowState state = _getStateByName(states, name);
+    if (state != null && revertState != null)
+      state.setPrevState(_getStateByName(states, revertState));
+    return state;
+  }
+
+  private static WorkflowState _getStateByName(List<WorkflowState> states,
+      String name) {
+    for (WorkflowState state : states) {
+      if (state.getName().toLowerCase().startsWith(name.toLowerCase()))
+        return state;
+      else if (state.getName().toLowerCase().equals(name.toLowerCase()))
+        return state;
+    }
+    return null;
+  }
+
+}
