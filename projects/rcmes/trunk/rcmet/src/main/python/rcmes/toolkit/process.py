d6953587aa9a614ced9433fc2f506e6f1ca550a9
cgoodale <cgoodale@unknown>
2012-09-12 13:43:33 +0000

    RCMES-67: Replaced decode_model_timesK with getModelTimes and moved the function into process.py
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl@1474095 13f79535-47bb-0310-9956-ffa450edef68

--- projects/rcmes/trunk/rcmet/src/main/python/rcmes/toolkit/process.py
+++ projects/rcmes/trunk/rcmet/src/main/python/rcmes/toolkit/process.py
@@ -523,96 +523,68 @@ def ignore_boundaries(data,rim=10):
 
    return data
 
-def decode_model_times(filelist,timeVarName):
-   '''
-    Routine to convert from model times ('hours since 1900...', 'days since ...') 
+def getModelTimes(modelFile,timeVarName):
+    '''
+    TODO:  Do a better job handling dates here
+    Routine to convert from model times ('hours since 1900...', 'days since ...')
     into a python datetime structure
-   
+
     Input::
-         filelist - list of model files
-         timeVarName - name of the time variable in the model files
-   
-     Output::
-         times  - list of python datetime objects describing model data times
-   '''
+        filelist - list of model files
+        timeVarName - name of the time variable in the model files
 
-   f = Nio.open_file(filelist[0])
-   xtimes = f.variables[timeVarName]
-   timeFormat = xtimes.attributes['units']
+    Output::
+        times  - list of python datetime objects describing model data times
+    '''
 
-   # search to check if 'since' appears in units
-   try:
-     sinceLoc = re.search('since',timeFormat).end()
+    f = Nio.open_file(modelFile)
+    xtimes = f.variables[timeVarName]
+    timeFormat = xtimes.attributes['units']
 
-   except:
-     print 'Error decoding model times: time variable attributes do not contain "since"'
-     return 0
+    # search to check if 'since' appears in units
+    try:
+        sinceLoc = re.search('since',timeFormat).end()
 
-   # search for 'seconds','minutes','hours', 'days', 'months', 'years' so know units
-   units = ''
-   try:
-     mysearch = re.search('minutes',timeFormat).end()
-     units = 'minutes'
-   except:
-     pass
-   try:
-     mysearch = re.search('hours',timeFormat).end()
-     units = 'hours'
-   except:
-     pass
-   try:
-     mysearch = re.search('days',timeFormat).end()
-     units = 'days'
-   except:
-     pass
-   try:
-     mysearch = re.search('months',timeFormat).end()
-     units = 'months'
-   except:
-     pass
-   try:
-     mysearch = re.search('years',timeFormat).end()
-     units = 'years'
-   except:
-     pass
-   
-   # cut out base time (the bit following 'since')
-   base_time_string = string.lstrip(timeFormat[sinceLoc:])
+    except AttributeError:
+        print 'Error decoding model times: time variable attributes do not contain "since"'
+        raise
 
-   # decode base time
-   base_time = decodeTimeFromString(base_time_string)
+    TIME_UNITS = ['minutes', 'hours','days','months','years']
+    # search for 'seconds','minutes','hours', 'days', 'months', 'years' so know units
+    for unit in TIME_UNITS:
+        if re.search(unit, timeFormat):
+            units = unit
 
+    # cut out base time (the bit following 'since')
+    base_time_string = string.lstrip(timeFormat[sinceLoc:])
+    # decode base time
+    base_time = decodeTimeFromString(base_time_string)
+    times=[]
 
-   times=[]
-   for xtime in xtimes[:]:
-      if(units=='minutes'):  
-         dt = datetime.timedelta(minutes=xtime)
-         new_time = base_time + dt
-
-      if(units=='hours'):  
-         dt = datetime.timedelta(hours=xtime)
-         new_time = base_time + dt
-
-      if(units=='days'):  
-         dt = datetime.timedelta(days=xtime)
-         new_time = base_time + dt
-
-      if(units=='months'):   # NB. adding months in python is complicated as month length varies and hence ambigous.
-         # Perform date arithmatic manually
-         #  Assumption: the base_date will usually be the first of the month
-         #              NB. this method will fail if the base time is on the 29th or higher day of month
-         #                      -as can't have, e.g. Feb 31st.
-         new_month = int(base_time.month + xtime % 12)
-         new_year = int(math.floor(base_time.year + xtime / 12.))
-         new_time = datetime.datetime(new_year,new_month,base_time.day,base_time.hour,base_time.second,0)
-
-      if(units=='years'):
-         dt = datetime.timedelta(years=xtime)
-         new_time = base_time + dt
-         
-      times.append(new_time)
-
-   return times
+    for xtime in xtimes[:]:
+        if units=='minutes':
+            dt = datetime.timedelta(minutes=xtime)
+            new_time = base_time + dt
+        if units=='hours':
+            dt = datetime.timedelta(hours=xtime)
+            new_time = base_time + dt
+        if units=='days':
+            dt = datetime.timedelta(days=xtime)
+            new_time = base_time + dt
+        if units=='months':
+            # NB. adding months in python is complicated as month length varies and hence ambiguous.
+            # Perform date arithmatic manually
+            #  Assumption: the base_date will usually be the first of the month
+            #              NB. this method will fail if the base time is on the 29th or higher day of month
+            #                      -as can't have, e.g. Feb 31st.
+            new_month = int(base_time.month + xtime % 12)
+            new_year = int(math.floor(base_time.year + xtime / 12.))
+            new_time = datetime.datetime(new_year,new_month,base_time.day,base_time.hour,base_time.second,0)
+        if units=='years':
+            dt = datetime.timedelta(years=xtime)
+            new_time = base_time + dt
+        times.append(new_time)
+    return times
 
 def decodeTimeFromString(time_string):
     '''
