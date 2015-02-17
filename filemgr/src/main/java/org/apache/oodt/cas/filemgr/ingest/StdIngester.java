5c0e6eb339c315ebfdf6a1417f5f77fb7f9fbd7b
Chris Mattmann <mattmann@apache.org>
2015-02-17 22:07:43 +0000

    - OODT-818 CmdLineIngester should log there was an exception and move on during ingest
    git-svn-id: https://svn.apache.org/repos/asf/oodt/trunk@1660516 13f79535-47bb-0310-9956-ffa450edef68

--- filemgr/src/main/java/org/apache/oodt/cas/filemgr/ingest/StdIngester.java
+++ filemgr/src/main/java/org/apache/oodt/cas/filemgr/ingest/StdIngester.java
@@ -108,26 +108,38 @@ public class StdIngester implements Ingester, CoreMetKeys {
         return ingest(fmUrl, prodFile, met);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.apache.oodt.cas.filemgr.ingest.Ingester#ingest(java.net.URL,
-     *      java.util.List, org.apache.oodt.cas.metadata.MetExtractor,
-     *      java.io.File)
-     */
-    public void ingest(URL fmUrl, List<String> prodFiles, MetExtractor extractor,
-            File metConfFile) throws IngestException {
-        if (prodFiles != null && prodFiles.size() > 0) {
-            for (Iterator<String> i = prodFiles.iterator(); i.hasNext();) {
-                String prodFilePath = i.next();
-                String productID = ingest(fmUrl, new File(prodFilePath),
-                        extractor, metConfFile);
-                LOG.log(Level.INFO, "Product: [" + prodFilePath
-                        + "] ingested successfully! ID: [" + productID + "]");
-            }
-        }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.oodt.cas.filemgr.ingest.Ingester#ingest(java.net.URL,
+	 * java.util.List, org.apache.oodt.cas.metadata.MetExtractor, java.io.File)
+	 */
+	public void ingest(URL fmUrl, List<String> prodFiles,
+			MetExtractor extractor, File metConfFile) throws IngestException {
+		if (prodFiles != null && prodFiles.size() > 0) {
+			for (Iterator<String> i = prodFiles.iterator(); i.hasNext();) {
+				String prodFilePath = i.next();
+				String productID = null;
 
-    }
+				try {
+					productID = ingest(fmUrl, new File(prodFilePath),
+							extractor, metConfFile);
+					LOG.log(Level.INFO, "Product: [" + prodFilePath
+							+ "] ingested successfully! ID: [" + productID
+							+ "]");
+				} catch (IngestException e) {
+					LOG.log(Level.WARNING,
+							"IngestException handling product: ["
+									+ prodFilePath
+									+ "]: Exception: ["
+									+ e.getMessage()
+									+ "]: Continuing ingest of remainder of products.");
+				}
+
+			}
+		}
+
+	}
 
     /*
      * (non-Javadoc)
