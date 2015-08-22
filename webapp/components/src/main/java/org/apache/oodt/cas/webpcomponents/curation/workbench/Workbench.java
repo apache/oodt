ecf9fa765e3f1d1f44c6fb35d50fb0b3d18217b0
Chris Mattmann <mattmann@apache.org>
2015-08-22 22:29:42 +0000

    - move Workbench to OODT web components: OODT-842: WIP
    git-svn-id: https://svn.apache.org/repos/asf/oodt/trunk@1697143 13f79535-47bb-0310-9956-ffa450edef68

--- webapp/components/src/main/java/org/apache/oodt/cas/webpcomponents/curation/workbench/Workbench.java
+++ webapp/components/src/main/java/org/apache/oodt/cas/webpcomponents/curation/workbench/Workbench.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.oodt.cas.curation.workbench;
+package org.apache.oodt.cas.webpcomponents.curation.workbench;
 
 import java.util.HashMap;
 import java.util.HashSet;
