30b3c9c26afe6eed4c4dac3916ef8dd56afafd99
Chris Mattmann <mattmann@apache.org>
2015-08-22 22:20:34 +0000

    OODT-842: WIP  CAS Curator Wicket
     - refactored the Workbench into its own Panel component (paving way for it to move to oodt-webapp-components)
     - next move: move Workbench panel to OODT webapp components
     
    
    git-svn-id: https://svn.apache.org/repos/asf/oodt/trunk@1697142 13f79535-47bb-0310-9956-ffa450edef68

--- curator/webapp/src/main/java/org/apache/oodt/cas/curation/workbench/Workbench.java
+++ curator/webapp/src/main/java/org/apache/oodt/cas/curation/workbench/Workbench.java
@@ -1,24 +1,59 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.oodt.cas.curation.workbench;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-import org.apache.wicket.markup.html.WebComponent;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.util.file.File;
 import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
 
-public class Workbench extends WebComponent{
+public class Workbench extends Panel {
 
+  private static final long serialVersionUID = 3911179455208050261L;
+
+  
   public Workbench(String id) {
     super(id);
   }
 
-  private static final long serialVersionUID = 3911179455208050261L;
 
   public static Set<String> getImageFiles() {
-    Pattern pattern = Pattern.compile(".*\\.png");
-    return new Reflections(Workbench.class.getPackage(), new ResourcesScanner())
+    Pattern pattern = Pattern.compile(".*\\.(png|gif)");
+    
+    Set<String> resources = new Reflections(Workbench.class.getPackage(), new ResourcesScanner())
         .getResources(pattern);
+    Set<String> filteredResources = new HashSet<String>();
+    Map<String, Boolean> resMap = new HashMap<String, Boolean>();
+    for(String res: resources){
+      String resName = new File(res).getName();
+      if (!resMap.containsKey(resName)){
+        resMap.put(resName, true);
+        filteredResources.add(resName);
+      }
+    }
+    
+    return filteredResources;
   }
 
 }
