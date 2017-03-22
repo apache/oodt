cfaafb59db87a5199a88f6f3717b1382bad34c7e
Sujen Shah <sujen1412@gmail.com>
2017-03-21 23:28:28 -0700

    Fix RDEV-553: Correct typo

--- curator2/src/main/java/org/apache/oodt/cas/curation/configuration/Configuration.java
+++ curator2/src/main/java/org/apache/oodt/cas/curation/configuration/Configuration.java
@@ -46,7 +46,7 @@ public class Configuration {
     public static final String S3_STAGING_AREA_CONFIG = "org.apache.oodt.cas.curator.s3.staging.area";
     public static final String S3_METADATA_KEY = "s3";
     public static final String LOCAL_METADATA_KEY = "local";
-    public static final String S3_DATA_TRANSFER_SERVICE = "org.apache.oodt.cas.filemgr.datatransfer.S3DataTransferFactory";
+    public static final String S3_DATA_TRANSFER_SERVICE = "org.apache.oodt.cas.filemgr.datatransfer.S3DataTransfererFactory";
     public static final String LOCAL_DATA_TRANSFER_SERVICE = "org.apache.oodt.cas.filemgr.datatransfer.LocalDataTransferFactory";
 
 
