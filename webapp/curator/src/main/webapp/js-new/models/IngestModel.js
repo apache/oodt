dbde1d7267db68cff1608d7cc592755dff6fc11d
Tom Barber <tom@analytical-labs.com>
2017-03-23 00:50:37 +0000

    push

--- webapp/curator/src/main/webapp/js-new/models/IngestModel.js
+++ webapp/curator/src/main/webapp/js-new/models/IngestModel.js
@@ -16,13 +16,14 @@ define(["jquery",
                 query += ((query == "")?"?":"&")+ "user="+GLOBAL_USER;
             }
           //var extractor = this.collection.extractors.get("selected");
-          //if (extractor != "" && extractor != null && typeof(extractor) !== "undefined")
-          //  if(extractor==="celgene-s3extractor"){
-          //    query += "&fsType=s3"
-          //  }
-          //  else{
-          //    query += "&fsType=local"
-          //  }
+          var extractor = $("#extractors").find(":selected").text();
+          if (extractor != "" && extractor != null && typeof(extractor) !== "undefined")
+            if(extractor==="celgene-s3extractor"){
+              query += "&fsType=s3"
+            }
+            else{
+              query += "&fsType=local"
+            }
 
           return Config.INGEST_REST_SERVICE + query;
         }
