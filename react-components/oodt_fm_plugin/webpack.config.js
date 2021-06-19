7ef08b0109e0fdc4085598749f2c9f7ccf9f52a6
pavinduLakshan <174084F@uom.lk>
2021-06-19 07:09:12 +0530

    add new configuration

--- react-components/oodt_fm_plugin/webpack.config.js
+++ react-components/oodt_fm_plugin/webpack.config.js
@@ -1,22 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use:{
-                    loader: "babel-loader",
-                },
-            },
-        ]
-    },
+    entry: path.join(__dirname, "src", "index.js"),
     output: {
         path: path.resolve(__dirname, 'dist/'),
         publicPath: '',
         filename: 'build.js',
         libraryTarget: 'umd'
+    },
+    mode: process.env.NODE_ENV || "development",
+    resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+    plugins: [],
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader?cacheDirectory=true',
+            }
+        }]
     },
     externals: {
         'react': "commonjs react",
